fd136f778162768adbf00834990939938cc217b3
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-07-01 00:08:48 +0530

    minor fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // allow cross-origin requests
 app.use(cors());
