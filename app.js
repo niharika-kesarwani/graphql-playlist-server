bbd8ec2ef9592f5c9e35d9dbc2eb89893603ce8b
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-30 22:57:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.port || 4000;
+
+// allow cross-origin requests
+app.use(cors());
+
+//connect to mlab database
+mongoose.connect(
+  "mongodb+srv://test:123@gql-playlist.ceervex.mongodb.net/?retryWrites=true&w=majority"
+);
+mongoose
+  .connect("open", () => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () =>
+  console.log(`now listening to requests on port ${PORT}`)
+);
